758e939bfb9b16fe7560b3d1a8b53f9c8a78f245
Kevin Lau <kelau1993@gmail.com>
2019-10-22 23:56:22 -0700

    Add initial preset code, tests, and documentation

---
+++ __tests__/const-object.js
@@ -0,0 +1,100 @@
+import { transformAsync } from '@babel/core';
+import preset from '../src';
+
+const options = {
+  filename: 'file.ts',
+  presets: [
+    [preset, { transform: 'constObject' }],
+  ],
+};
+
+it('Transforms no initializers', async () => {
+  const input =
+`const enum Direction { Left, Right, Down, Up };
+`;
+
+  const { code: output } = await transformAsync(input, options);
+  expect(output).toMatchSnapshot();
+});
+
+it('Transforms string members', async () => {
+  const input =
+`const enum Enum {
+  A = 1,
+  B = A,
+  C = '',
+  D = C,
+  E = 1,
+  F,
+}
+`;
+
+  const { code: output } = await transformAsync(input, options);
+  expect(output).toMatchSnapshot();
+});
+
+it('Transforms computed members', async () => {
+  const input =
+`const enum MyEnum {
+  A = 1,
+  B = A,
+  C,
+  D = C,
+  E = 1,
+  F,
+  G = A * E,
+  H = A ** B ** C,
+  I = A << 20
+}
+`;
+
+  const { code: output } = await transformAsync(input, options);
+  expect(output).toMatchSnapshot();
+
+  const MyEnum = (new Function(
+`${output}
+return MyEnum;
+`))();
+  expect(MyEnum.A).toBe(1);
+  expect(MyEnum.B).toBe(1);
+  expect(MyEnum.C).toBe(2);
+  expect(MyEnum.D).toBe(2);
+  expect(MyEnum.E).toBe(1);
+  expect(MyEnum.F).toBe(2);
+  expect(MyEnum.G).toBe(1);
+  expect(MyEnum.H).toBe(1);
+  expect(MyEnum.I).toBe(1048576);
+});
+
+it('Transforms chained computed members', async () => {
+  const input =
+`const enum MyEnum {
+  A = 1,
+  B = A * 2,
+  C,
+  D = C,
+  E = D ** 2,
+  F,
+  G = F * E,
+  H,
+  I = H << 20
+}
+`;
+
+  const { code: output } = await transformAsync(input, options);
+  expect(output).toMatchSnapshot();
+
+  const MyEnum = (new Function(
+`${output}
+return MyEnum;
+`))();
+  expect(MyEnum.A).toBe(1);
+  expect(MyEnum.B).toBe(2);
+  expect(MyEnum.C).toBe(3);
+  expect(MyEnum.D).toBe(3);
+  expect(MyEnum.E).toBe(9);
+  expect(MyEnum.F).toBe(10);
+  expect(MyEnum.G).toBe(90);
+  expect(MyEnum.H).toBe(91);
+  expect(MyEnum.I).toBe(95420416);
+});
---
+++ __tests__/extensions.js
@@ -0,0 +1,35 @@
+import { transformAsync } from '@babel/core';
+import preset from '../src';
+
+const options = {
+  presets: [preset],
+};
+
+  const input = `const enum Direction { Left, Right, Down, Up };
+`;
+
+it('Transforms *.ts', async () => {
+  const { code: output } = await transformAsync(input, {
+    ...options,
+    filename: 'file.ts',
+  });
+  expect(output).toMatchSnapshot();
+});
+
+it('Transforms *.tsx', async () => {
+  const { code: output } = await transformAsync(input, {
+    ...options,
+    filename: 'file.tsx',
+  });
+  expect(output).toMatchSnapshot();
+});
+
+it('Transforms *.*', async () => {
+  const { code: output } = await transformAsync(input, {
+    filename: 'file.js',
+    presets: [
+      [preset, { allExtensions: true }],
+    ],
+  });
+  expect(output).toMatchSnapshot();
+});
